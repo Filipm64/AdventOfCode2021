53588f2afa416df0f7558eaf60bf90aba873faed
Filipm64 <filip.matus64@gmail.com>
2021-12-09 22:14:31 +0100

    day1 complete

--- day1.py
+++ day1.py
@@ -1,17 +1,44 @@
-f = open("day1.txt", "r")
-
-line = int(f.readline())
-previousLine = line
-count = 0
-
-while line != "":
-
-    line = int(line)
-
-    if line > previousLine:
-        count = count + 1
-
+def partOne():
+    f = open("day1.txt", "r")
+    line = int(f.readline())
     previousLine = line
-    line = f.readline()
-    
-print("Count = ", count)
+    count = 0
+
+    while line != "":
+
+        line = int(line)
+
+        if line > previousLine:
+            count = count + 1
+
+        previousLine = line
+        line = f.readline()
+        
+    print("Count part one = ", count)
+
+def partTwo():
+    fPartTwo = open("day1.txt", "r")
+    line = int(fPartTwo.readline())
+    line2 = int(fPartTwo.readline())
+    line3 = int(fPartTwo.readline())
+    line4 = int(fPartTwo.readline())
+    count = 0
+
+    while line4 != "":
+
+        line4 = int(line4)
+        sum1 = line + line2 + line3
+        sum2 = line2 + line3 + line4
+
+        if sum1 < sum2:
+            count = count + 1
+
+        line = line2
+        line2 = line3
+        line3 = line4
+        line4 = fPartTwo.readline()
+        
+    print("Count  part two = ", count)
+
+partOne()
+partTwo()
